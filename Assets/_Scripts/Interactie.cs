94ea90c019f15a173233e7172422827a01b6eb76
tommtom9 <tommtom9@live.nl>
2016-02-12 10:48:05 +0100

    Proof of concept

---
+++ Assets/_Scripts/Interactie.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactie : MonoBehaviour
+{
+
+    void OnCollisionStay2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == Tags.Lever)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                coll.gameObject.GetComponent<Renderer>().material.color = new Color(1, 0, 0, 0);
+            }
+                            
+        }
+
+    }
+
+}
