0884f8bdc212d5ef9f1e4d399f95046c09c383c3
tommtom9 <tommtom9@live.nl>
2016-03-07 11:40:34 +0100

    Updated greyboxing

---
+++ Assets/_Scripts/AttackScript/BulletRain/BulletRain.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletRain : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject bullet;
+
+    public void Attack(int maxWidthtOffset, int minWidthtOffset, int heightOffset, int amount, Vector3 bulletSpawnpoint)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            Instantiate(bullet, new Vector3(bulletSpawnpoint.x + Random.Range(minWidthtOffset, maxWidthtOffset), bulletSpawnpoint.y + heightOffset), Quaternion.identity);
+            
+        }
+    }
+}
--- Assets/_Scripts/Boss1.cs
+++ Assets/_Scripts/Boss1.cs
@@ -1,25 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Boss1 : MonoBehaviour {
 
     private Vector2 bulletSpawnPoint;
     private BulletStorm bulletStorm;
-    private int loopTime = 5;
+    private BulletRain bulletRain;
+    private int randomNumber;
+
+    [SerializeField]
+    private int loopTime = 1;
 
     [SerializeField]
     private int bulletAmount = 10;
 
+    private GameObject attackStartTrigger;
+
 	void Start () {
         bulletSpawnPoint = GameObject.Find(Tags.Bulletpoint).transform.position;
         bulletStorm = GetComponent<BulletStorm>();
-        StartCoroutine(Loop(loopTime));
+        bulletRain = GetComponent<BulletRain>();
+
+        attackStartTrigger = GameObject.Find(Tags.b0ssTrigger);
+
+        startAttack();
 	}
+     
+    public void startAttack()
+    {
+        StartCoroutine(Loop(loopTime));
+    }
 	
 	IEnumerator Loop(float loopTime)
     {
-        bulletStorm.Attack(bulletSpawnPoint, bulletAmount);
-        yield return new WaitForSeconds(loopTime);
+        randomNumber = Random.Range(0, 2);
+
+        if (randomNumber == 0)
+        {
+            bulletStorm.Attack(bulletSpawnPoint, bulletAmount);
+        }
+
+        else if (randomNumber == 1)
+        {
+            bulletRain.Attack(30, -30, 40, 10, bulletSpawnPoint);
+        }
+
+            yield return new WaitForSeconds(loopTime);
         StartCoroutine(Loop(loopTime));
     }
 }
