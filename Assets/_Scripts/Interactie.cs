97252ee5ac55f0e47c1cc8705cc56996593cf27b
Swan Chase <swancd@gmail.com>
2016-03-08 11:24:17 +0100

    Timer start and door start
    using delegates

--- Assets/_Scripts/Interactie.cs
+++ Assets/_Scripts/Interactie.cs
@@ -1,8 +1,17 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
+using System;
 
 public class Interactie : MonoBehaviour
 {
+    public delegate void TimerStarter(int time);
+    public TimerStarter StartTimer;
+
+    /*private void DoorTimer(int time)
+    {
+        Debug.Log("derp" + time);
+    }*/
 
     void OnTriggerStay2D(Collider2D coll)
     {
@@ -13,9 +22,16 @@ public class Interactie : MonoBehaviour
                 coll.gameObject.GetComponent<Renderer>().material.color = new Color(21, 10, 38, 1);
                 Destroy(GameObject.Find("delete"));
             }
-                            
+
         }
-
+        if (coll.gameObject.tag == Tags.Lever)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                StartTimer(10);
+                coll.gameObject.GetComponent<Renderer>().material.color = new Color(21, 10, 38, 1);
+                Destroy(GameObject.Find("delete"));
+            }
+        }
     }
-
 }
--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -7,9 +7,11 @@ public class PlayerController : MonoBehaviour
     public float speed = 0f, JumpVelocity = 10;
     public LayerMask playerMask;
     public bool canMoveInAir = true;
-    Transform myTrans, tagGround;
+    Transform myTrans, tagGround, tagLeft, tagRight;
     Rigidbody2D rb;
     bool isGrounded = false;
+    //bool isOnLeftWall = false;
+    //bool isOnRightWall = false;
 
     // Use this for initialization
     void Start()
@@ -17,6 +19,9 @@ public class PlayerController : MonoBehaviour
         myTrans = gameObject.GetComponent<Transform>();
         rb = gameObject.GetComponent<Rigidbody2D>();
         tagGround = GameObject.Find(this.name + "/tag_ground").transform;
+        //tagLeft = GameObject.Find(this.name + "/tag_left").transform;
+        //tagRight = GameObject.Find(this.name + "/tag_right").transform;
+
     }
 
     // Update is called once per frame
@@ -24,6 +29,8 @@ public class PlayerController : MonoBehaviour
     {
         //
         isGrounded = Physics2D.Linecast(myTrans.position, tagGround.position, playerMask);
+        //isOnLeftWall = Physics2D.Linecast(myTrans.position, tagLeft.position, playerMask);
+        //isOnRightWall = Physics2D.Linecast(myTrans.position, tagRight.position, playerMask);
 
         Move(Input.GetAxis("Horizontal"));
         if (Input.GetButtonDown("Jump"))
@@ -31,6 +38,7 @@ public class PlayerController : MonoBehaviour
             Jump();
         }
     }
+
     void Move(float horizontalInput)
     {
         if (!canMoveInAir && !isGrounded)
@@ -44,7 +52,7 @@ public class PlayerController : MonoBehaviour
 
     public void Jump()
     {
-        if (isGrounded)
+        if (isGrounded /*|| isOnLeftWall || isOnRightWall*/)
         {
             rb.velocity += JumpVelocity * Vector2.up/*,ForceMode2D.Impulse*/;
         }
