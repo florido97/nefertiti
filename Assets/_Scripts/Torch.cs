c818fab4afe35509e45143ead75b4a9df75b0d27
tommtom9 <tommtom9@live.nl>
2016-03-09 15:48:03 +0100

    Updates on greybox, adding art soon

---
+++ Assets/_Scripts/Torch.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torch : MonoBehaviour {
+
+    Light light;
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        StartCoroutine(lightLoop());
+    }
+
+    IEnumerator lightLoop()
+    {
+        light.intensity = Random.Range(3.6f, 4f);
+        yield return new WaitForSeconds(0.01f);
+        StartCoroutine(lightLoop());
+    }
+}
