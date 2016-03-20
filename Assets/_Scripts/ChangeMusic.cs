1c667fa1bd9c1a18e34ce23ee9479b163d372d7f
Swan Chase <swancd@gmail.com>
2016-03-21 00:18:58 +0100

    Added Menu, pauseMenu, levelSelect level and level "Doors"

---
+++ Assets/_Scripts/ChangeMusic.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Change : MonoBehaviour
+{
+    public AudioClip level2Music;
+
+    private AudioSource source;
+
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level == 2)
+        {
+            source.clip = level2Music;
+            source.Play();
+        }
+    }
+
+}
