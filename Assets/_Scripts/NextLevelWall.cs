6fe63a8483d1dc3f0f8360e191ab23cd26429b56
tommtom9 <tommtom9@live.nl>
2016-03-25 11:25:44 +0100

    More of Swan's shit

--- Assets/_Scripts/NextLevelWall.cs
+++ Assets/_Scripts/NextLevelWall.cs
@@ -6,13 +6,22 @@ public class NextLevelWall : MonoBehaviour
 {
     public int nextLevelNumber;
 
+<<<<<<< HEAD
     PlayerInteraction interAction;
+=======
+    PlayerInteraction playerInterAction;
+>>>>>>> f420e9374b74cf17c0eb6c6bc451ad4c0dda7971
 
     // Use this for initialization
     void Start()
     {
+<<<<<<< HEAD
         interAction = GameObject.Find("Player").GetComponent<PlayerInteraction>();
         interAction.NextLevel += NextLevel;
+=======
+        playerInterAction = GameObject.Find("Player").GetComponent<PlayerInteraction>();
+        playerInterAction.NextLevel += NextLevel;
+>>>>>>> f420e9374b74cf17c0eb6c6bc451ad4c0dda7971
     }
     // Update is called once per frame
     void Update()
