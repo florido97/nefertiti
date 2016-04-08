87660dbb00261200eab7f8f7dfd10aaccb93d96c
tommtom9 <tommtom9@live.nl>
2016-04-08 22:36:21 +0200

    more comments uknow

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -3,26 +3,42 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-
+    //Floats for the speed, JumpVelocity and time invincibilty after getting hit
     public float speed = 0f, JumpVelocity = 10, invincibleTimeAfterHurt = 3;
+
+    //The layers the player can interact with
     public LayerMask playerMask;
+
+    //A bool the determins if the player can control the player sprite in the air
     public bool canMoveInAir = true;
 
+    //Transforms used to ground detection
     Transform myTrans, tagGround, tagLeft, tagRight;
+
+    //The rigidbody of the players object
     Rigidbody2D rb;
+
+    //The particleSystem on the player
     GameObject particleSys;
+
+    //All the players colliders
     Collider2D[] myColls;
 
+    //The bools used for ground detection
     bool isGrounded = false, isOnLeft = false, isOnRight = false;
+
+    //The animator the player uses, on the player sprite
     Animator ani;
 
     void Start()
     {
+        //Getting all the diffrent componets of the player
         myColls = gameObject.GetComponents<Collider2D>();
         myTrans = gameObject.GetComponent<Transform>();
         rb = gameObject.GetComponent<Rigidbody2D>();
         ani = GetComponentInChildren<Animator>();
 
+        //Searching for the ground detection tags.
         tagGround = GameObject.Find(this.name + "/tag_Ground").transform;
         tagLeft = GameObject.Find(this.name + "/tag_Left").transform;
         tagRight = GameObject.Find(this.name + "/tag_Right").transform;
@@ -33,6 +49,8 @@ public class PlayerController : MonoBehaviour
         isGrounded = Physics2D.Linecast(myTrans.position, tagGround.position, playerMask);
         isOnLeft = Physics2D.Linecast(myTrans.position, tagLeft.position, playerMask);
         isOnRight = Physics2D.Linecast(myTrans.position, tagRight.position, playerMask);
+
+
         Move(Input.GetAxis("Horizontal"));
 
         float h = Input.GetAxis("Horizontal");
